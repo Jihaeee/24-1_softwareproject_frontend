afd238847c9b386c25f50bd0b8794181cb47709d
Jihaeee <elinkang02@gmail.com>
2024-05-23 14:37:01 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./LoginPage";
-import SignupPage from "./SignUpPage";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import UserPage from "./UserPage";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+import IssuePage from './IssuePage';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<UserPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Register</Link>
+            </li>
+            <li>
+            <Link to="/create-issue">Create Issue</Link>
+          </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<UserPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/create-issue" element={<IssuePage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
